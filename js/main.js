6a7363e910afac939399b1675bddc1e264d67a68
Matt Smith <runner_28@hotmail.com>
2016-03-28 00:04:30 -0400

    Refactored CDN load check 🚀

---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function ($) {
+  var $span = $('<span class="fa" style="display: none;"></span>').appendTo('body');
+  if ($span.css('fontFamily') !== 'FontAwesome') {
+    // local fallback
+    $('head').append('<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">');
+  }
+  $span.remove();
+}(jQuery));
