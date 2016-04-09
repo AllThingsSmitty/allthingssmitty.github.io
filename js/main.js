b8818cd98b5a51dbdeab01015b969f233024958c
Matt Smith <runner_28@hotmail.com>
2016-04-09 10:23:32 -0400

    Updated path to assets 🙌🏼

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
   var $span = $('<span class="fa" style="display: none;"></span>').appendTo('body');
   if ($span.css('fontFamily') !== 'FontAwesome') {
     // local fallback
-    $('head').append('<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">');
+    $('head').append('<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">');
   }
   $span.remove();
 }(jQuery));
