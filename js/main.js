8dab5c36b6d4d8c9d86f56d9fed5a8212965725f
Matt Smith <runner_28@hotmail.com>
2016-04-14 09:03:55 -0400

    Linting is good for the soul 😊

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-window.onload =
-  function () {
-    var span = document.createElement('span'),
-      headHTML = document.head.innerHTML;
+window.onload = function () {
+  var span = document.createElement('span'),
+    headHTML = document.head.innerHTML;
 
-    span.className = 'fa';
-    span.style.display = 'none';
-    document.body.insertBefore(span, document.body.firstChild);
-    if (span.style.fontFamily !== 'FontAwesome') {
-      headHTML += '<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">';
-      document.head.innerHTML = headHTML;
-    }
-    document.body.removeChild(span);
-  };
+  span.className = 'fa';
+  span.style.display = 'none';
+  document.body.insertBefore(span, document.body.firstChild);
+  if (span.style.fontFamily !== 'FontAwesome') {
+    headHTML += '<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">';
+    document.head.innerHTML = headHTML;
+  }
+  document.body.removeChild(span);
+};
