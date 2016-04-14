a65b162dae7ddc3f825330cc05da56ddf16cfba3
Matt Smith <runner_28@hotmail.com>
2016-04-13 23:14:25 -0400

    Refactored vanilla JS 🍦

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-(function ($) {
-  var $span = $('<span class="fa" style="display: none;"></span>').appendTo('body');
-  if ($span.css('fontFamily') !== 'FontAwesome') {
-    // local fallback
-    $('head').append('<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">');
-  }
-  $span.remove();
-}(jQuery));
+window.onload =
+  function () {
+    var span = document.createElement('span'),
+      headHTML = document.head.innerHTML;
+
+    span.className = 'fa';
+    span.style.display = 'none';
+    document.body.insertBefore(span, document.body.firstChild);
+    if (span.style.fontFamily !== 'FontAwesome') {
+      headHTML += '<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">';
+      document.head.innerHTML = headHTML;
+    }
+    document.body.removeChild(span);
+  };
