e6717535d97790a5ed3c12df25ad6194b12e2f92
Matt Smith <runner_28@hotmail.com>
2016-04-22 16:58:01 -0400

    Refactored to use jQuery 🚀

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ window.onload = function () {
   }
   document.body.removeChild(span);
 };
+
+
+/*
+
+(function ($) {
+  var $span = $('<span class="fa" style="display: none;"></span>').appendTo('body');
+  if ($span.css('fontFamily') !== 'FontAwesome') {
+    // local fallback
+    $('head').append('<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">');
+  }
+  $span.remove();
+}(jQuery));
+
+*/
