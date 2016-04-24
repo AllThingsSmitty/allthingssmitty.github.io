cb58725be214ca73cc65709353cfe1ee583bc0fd
Matt Smith <runner_28@hotmail.com>
2016-04-24 09:59:53 -0400

    Added function to get font-family 😎

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function css(element, property) {
+  return window.getComputedStyle(element, null).getPropertyValue(property);
+}
+
 window.onload = function () {
   var span = document.createElement('span'),
     headHTML = document.head.innerHTML;
@@ -5,23 +9,12 @@ window.onload = function () {
   span.className = 'fa';
   span.style.display = 'none';
   document.body.insertBefore(span, document.body.firstChild);
-  if (span.style.fontFamily !== 'FontAwesome') {
+  if ((css(span, 'font-family')) !== 'FontAwesome') {
     headHTML += '<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">';
     document.head.innerHTML = headHTML;
+  } else {
+    return;
   }
   document.body.removeChild(span);
+  return;
 };
-
-
-/*
-
-(function ($) {
-  var $span = $('<span class="fa" style="display: none;"></span>').appendTo('body');
-  if ($span.css('fontFamily') !== 'FontAwesome') {
-    // local fallback
-    $('head').append('<link rel="stylesheet" href="third-party/font-awesome/css/font-awesome.min.css">');
-  }
-  $span.remove();
-}(jQuery));
-
-*/
