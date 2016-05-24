d231eec188713fc36ad424ab11cacb1681bbb36a
Matt Smith <runner_28@hotmail.com>
2016-05-23 20:42:39 -0400

    Added check for fonts loading 🙌🏼

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
+// check for when the fonts are finished loading and add the `fonts-loaded` class to the HTML tag
+(function (w) {
+  if (w.document.documentElement.className.indexOf('fonts-loaded') > -1) {
+    return;
+  }
+  var slabo = new w.FontFaceObserver('Slabo27px', {
+    weight: 500
+  });
+
+  w.Promise
+    .all([slabo.check()])
+    .then(function () {
+      w.document.documentElement.className += ' fonts-loaded';
+    });
+}(this));
+
+
+// get the computed style property and value of an element
 function css(element, property) {
   return window.getComputedStyle(element, null).getPropertyValue(property);
 }
 
+
+// add a <span> to the start of the body and check if `FontAwesome` is the font-family
 window.onload = function () {
   var span = document.createElement('span'),
     headHTML = document.head.innerHTML;
