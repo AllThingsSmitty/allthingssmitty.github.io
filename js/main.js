b22558791a8cacdc52325e8db559de3c69ec46de
Matt Smith <runner_28@hotmail.com>
2016-11-30 19:57:02 -0500

    Added font loading

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
-
-// check if Font Awesome CDN loaded
 (function () {
-  var span = document.createElement('span'),
-    headHTML = document.head.innerHTML;
-
-  span.className = 'fa';
-  span.style.display = 'none';
-  document.body.insertBefore(span, document.body.firstChild);
-
-  // get the computed style property and value of an element
-  function css(element, property) {
-    return window.getComputedStyle(element, null).getPropertyValue(property);
+  'use strict';
+  if (document.documentElement.className.indexOf('fonts-loaded') > -1) {
+    return;
   }
-
-  if (css(span, 'font-family') !== 'FontAwesome') {
-    headHTML += '<link rel="stylesheet" href="/third-party/font-awesome/css/font-awesome.min.css">';
-    document.head.innerHTML = headHTML;
-  }
-  document.body.removeChild(span);
-  return;
+  var BitterRegular = new FontFaceObserver('BitterRegular'),
+    BitterItalic = new FontFaceObserver('BitterItalic'),
+    BitterBold = new FontFaceObserver('BitterBold'),
+    BitterBoldItalic = new FontFaceObserver('BitterBoldItalic'),
+    OpenSansRegular = new FontFaceObserver('OpenSansRegular'),
+    OpenSansSemibold = new FontFaceObserver('OpenSansSemibold'),
+    OpenSansBold = new FontFaceObserver('OpenSansBold'),
+    Inconsolata = new FontFaceObserver('Inconsolata');
+  Promise.all([
+    BitterRegular.load(),
+    BitterItalic.load(),
+    BitterBold.load(),
+    BitterBoldItalic.load(),
+    OpenSansRegular.load(),
+    OpenSansSemibold.load(),
+    OpenSansBold.load(),
+    Inconsolata.load()
+  ]).then(function () {
+    document.documentElement.className += 'fonts-loaded';
+    console.log('All fonts loaded');
+  });
 }());
