8800334c2b3b424d17c0a9a62e74c5e7e7bdf138
Matt Smith <runner_28@hotmail.com>
2017-06-07 06:43:47 -0400

    Dynamically loading FontFaceObserver

--- js/main.js
+++ js/main.js
@@ -1,44 +1,52 @@
-document.addEventListener('DOMContentLoaded', function () {
-  'use strict';
-  if (document.documentElement.className.indexOf('fonts-loaded') > -1) {
-    return;
-  }
-  var BitterRegular = new FontFaceObserver('BitterRegular', {
-    weight: 400
-  }),
-    BitterItalic = new FontFaceObserver('BitterItalic', {
+var script = document.createElement('script');
+
+script.async = true;
+script.src = '/js/fontfaceobserver.min.js';
+script.onload = function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    if (document.documentElement.className.indexOf('fonts-loaded') > -1) {
+      return;
+    }
+    
+    var BitterRegular = new FontFaceObserver('BitterRegular', {
       weight: 400
     }),
-    BitterBold = new FontFaceObserver('BitterBold', {
-      weight: 700
-    }),
-    BitterBoldItalic = new FontFaceObserver('BitterBoldItalic', {
-      weight: 700
-    }),
-    OpenSansRegular = new FontFaceObserver('OpenSansRegular', {
-      weight: 400
-    }),
-    OpenSansSemibold = new FontFaceObserver('OpenSansSemibold', {
-      weight: 600
-    }),
-    OpenSansBold = new FontFaceObserver('OpenSansBold', {
-      weight: 700
-    }),
-    Inconsolata = new FontFaceObserver('Inconsolata', {
-      weight: 400
+      BitterItalic = new FontFaceObserver('BitterItalic', {
+        weight: 400
+      }),
+      BitterBold = new FontFaceObserver('BitterBold', {
+        weight: 700
+      }),
+      BitterBoldItalic = new FontFaceObserver('BitterBoldItalic', {
+        weight: 700
+      }),
+      OpenSansRegular = new FontFaceObserver('OpenSansRegular', {
+        weight: 400
+      }),
+      OpenSansSemibold = new FontFaceObserver('OpenSansSemibold', {
+        weight: 600
+      }),
+      OpenSansBold = new FontFaceObserver('OpenSansBold', {
+        weight: 700
+      }),
+      Inconsolata = new FontFaceObserver('Inconsolata', {
+        weight: 400
+      });
+
+    Promise.all([
+      BitterRegular.load(),
+      BitterItalic.load(),
+      BitterBold.load(),
+      BitterBoldItalic.load(),
+      OpenSansRegular.load(),
+      OpenSansSemibold.load(),
+      OpenSansBold.load(),
+      Inconsolata.load()
+    ]).then(function () {
+      document.documentElement.className += 'fonts-loaded';
+      console.log('All fonts loaded');
     });
-  
-  Promise.all([
-    BitterRegular.load(),
-    BitterItalic.load(),
-    BitterBold.load(),
-    BitterBoldItalic.load(),
-    OpenSansRegular.load(),
-    OpenSansSemibold.load(),
-    OpenSansBold.load(),
-    Inconsolata.load()
-  ]).then(function () {
-    document.documentElement.className += 'fonts-loaded';
-    console.log('All fonts loaded');
-  });
-}());
+  }());
+
+};
+document.head.appendChild(script);
