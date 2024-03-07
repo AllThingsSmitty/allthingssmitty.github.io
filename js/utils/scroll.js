1d80c9790e1d66975cfafcc49364a1cdd82ebebe
Matt Smith <runner_28@hotmail.com>
2024-03-07 16:50:01 -0500

    Error handling

--- js/utils/scroll.js
+++ js/utils/scroll.js
@@ -1,24 +1,28 @@
 // scroll
 {
-  const scrollToTopBtn = document.querySelector('.scrollToTop'),
-  rootElement = document.documentElement;
-  
+  const scrollToTopBtn = document.querySelector(".scrollToTop");
+  const rootElement = document.documentElement;
+ 
   const handleScroll = () => {
     const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-    scrollToTopBtn.classList.toggle('showBtn', rootElement.scrollTop / scrollTotal > 0.3);
-  }
-  
+    scrollToTopBtn.classList.toggle(
+      "showBtn",
+      rootElement.scrollTop / scrollTotal > 0.3
+    );
+  };
+ 
   const scrollToTop = () => {
     rootElement.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
-  }
-  
+  };
+ 
   try {
-    scrollToTopBtn.addEventListener('click', scrollToTop);
-    document.addEventListener('scroll', handleScroll);
+    scrollToTopBtn.addEventListener("click", scrollToTop);
+    document.addEventListener("scroll", handleScroll);
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error(`An error occurred: ${error}`);
   }
 }
+ 
