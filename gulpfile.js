0acc5e1358beef7363c15ba3a537029eee5004b9
Matt Smith <runner_28@hotmail.com>
2024-04-22 11:49:46 -0400

    Add exception handling

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,61 @@ const uglify = require("gulp-uglify");
 const cp = require("child_process");
 const browserSync = require("browser-sync").create();
 
-function clean() {
-  return del(["./_site", ".sass-cache", "./js/**/*.min.js"]);
+async function clean() {
+  try {
+    console.log("just deleted the directories");
+    return await del(["./_site", ".sass-cache", "./js/**/*.min.js"]);
+  } catch (error) {
+    console.error(`Error in clean: ${error}`);
+  }
 }
 
-function scripts() {
-  return src(["./js/**/*.js"]).pipe(concat("main.min.js")).pipe(uglify()).pipe(dest("./js"));
+async function scripts() {
+  try {
+    return src(["./js/**/*.js"]).pipe(concat("main.min.js")).pipe(uglify()).pipe(dest("./js"));
+  } catch (error) {
+    console.error(`Error in scripts: ${error}`);
+  }
 }
 
 function jekyll(done) {
-  cp.spawn("jekyll", ["build"], { stdio: "inherit" }).on("close", done);
+  try {
+    cp.spawn("jekyll", ["build"], { stdio: "inherit" }).on("close", done);
+  } catch (error) {
+    console.error(`Error in jekyll: ${error}`);
+  }
 }
 
 function serve(done) {
-  browserSync.init({
-    server: "./_site",
-  });
-  done();
+  try {
+    browserSync.init({
+      server: "./_site",
+    });
+    done();
+  } catch (error) {
+    console.error(`Error in serve: ${error}`);
+  }
 }
 
 function reload(done) {
-  browserSync.reload();
-  done();
+  try {
+    browserSync.reload();
+    done();
+  } catch (error) {
+    console.error(`Error in reload: ${error}`);
+  }
 }
 
 const watcher = () => {
-  watch(
-    ["./_sass/*", "./js/*/*.js", "*.html", "./_includes/*html", "./_layouts/*.html", "./_posts/*"],
-    series(scripts, jekyll, reload),
-  );
+  try {
+    watch(
+      ["./_sass/*", "./js/*/*.js", "*.html", "./_includes/*html", "./_layouts/*.html", "./_posts/*"],
+      series(scripts, jekyll, reload),
+    );
+    console.error("watching...");
+  } catch (error) {
+    console.error(`Error in watcher: ${error}`);
+  }
 };
 
 exports.default = series(clean, scripts, jekyll, serve, watcher);
